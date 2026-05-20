ab301f5e2b3f9dc44a4127f74207e9579210b72f
Ellisalean <148759595+Ellisalean@users.noreply.github.com>
2026-05-20 13:39:25 -0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/SpiritHarmonize/',
+    base: '/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
